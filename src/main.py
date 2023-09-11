ed62f4262fa841cec3ff921b811c05af2a95f76e
Anjurad <anjurad@hotmail.com>
2023-09-11 09:19:47 +0100

    building out v2 scenario

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+
 # imports
 import os
 import mlflow
@@ -22,7 +23,7 @@ def main(args):
     }
 
     # read in data
-    df = pd.read_csv(args.diabetes_csv)
+    df = pd.read_csv(args.data)
 
     # process data
     X_train, X_test, y_train, y_test = process_data(df, args.random_state)
@@ -30,6 +31,8 @@ def main(args):
     # train model
     model = train_model(params, X_train, X_test, y_train, y_test)
 
+    # Stop Logging
+    # mlflow.end_run()
 
 def process_data(df, random_state):
     # split dataframe into X and y
@@ -44,33 +47,45 @@ def process_data(df, random_state):
     # return splits and encoder
     return X_train, X_test, y_train, y_test
 
-
 def train_model(params, X_train, X_test, y_train, y_test):
     # train model
     model = LinearRegression(**params)
     model = model.fit(X_train, y_train)
 
+    # Registering the model to the workspace
+    print("Registering the model via MLFlow")
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        registered_model_name=args.registered_model_name,
+        artifact_path=args.registered_model_name,
+    )
+
+    # Saving the model to a file
+    mlflow.sklearn.save_model(
+        sk_model=model,
+        path=os.path.join(args.registered_model_name, "trained_model"),
+    )
+
     # return model
     return model
-
 
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
 
     # add arguments
-    parser.add_argument("--diabetes-csv", type=str)
+    parser.add_argument("--data", type=str)
     parser.add_argument("--random_state", type=int, default=42)
     parser.add_argument("--fit_intercept", type=bool, default=True)
     parser.add_argument("--normalize", type=bool, default=False)
     parser.add_argument("--positive", type=bool, default=False)
+    parser.add_argument("--registered_model_name", type=str, help="model name")
 
     # parse args
     args = parser.parse_args()
 
     # return args
     return args
-
 
 # run script
 if __name__ == "__main__":
